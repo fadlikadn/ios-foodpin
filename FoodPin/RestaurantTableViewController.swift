42d169599456b8184f6547a784e524901a5f3382
Fadlika Dita Nurjanto <fadlikadita.nurjanto@mitrais.com>
2017-07-10 14:40:52 +0700

    Add call and checkIn action

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -61,9 +61,44 @@ class RestaurantTableViewController: UITableViewController {
         cell.thumbnailImageView.layer.cornerRadius = 30.0
         cell.thumbnailImageView.clipsToBounds = true
         
-        cell.backgroundImageView.image = UIImage(named: restaurantImages[indexPath.row])
+//        cell.backgroundImageView.image = UIImage(named: restaurantImages[indexPath.row])
+//        cell.backgroundImageView.tintColor = UIColor(red: 8, green: 6, blue: 23, alpha: 1.0)
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Create an option menu as an action sheet
+        let optionMenu  = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        // Add Call action
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        // Check-in action
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+        })
+        optionMenu.addAction(checkInAction)
+        
+        // Add cancel actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        // Display the menu
+        present(optionMenu, animated: true, completion: nil)
+        
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 
     /*
