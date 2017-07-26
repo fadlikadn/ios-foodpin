1e6dee339fb960231ef839c419c2e959e10fdd85
Fadlika Dita Nurjanto <fadlikadita.nurjanto@mitrais.com>
2017-07-26 10:47:51 +0700

    Add blur image background in review detail view

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -93,6 +93,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         return cell
     }
     
+    @IBAction func close(segue:UIStoryboardSegue) {
+        
+    }
+    
 
     /*
     // MARK: - Navigation
